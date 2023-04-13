7622960cdecbb213daac4425513a5ed895d9dba1
tharuka-devinda <tharukadevinda96@gmail.com>
2023-04-14 01:32:04 +0530

    UI Complete

---
+++ src/main/java/lk/ijse/dep10/students/controller/TeachersDetailViewController.java
@@ -0,0 +1,117 @@
+package lk.ijse.dep10.students.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class TeachersDetailViewController {
+
+    @FXML
+    private Button btnAddContact;
+
+    @FXML
+    private Button btnAddSubject;
+
+    @FXML
+    private Button btnChange;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnDeleteContact;
+
+    @FXML
+    private Button btnDeleteSubject;
+
+    @FXML
+    private Button btnNewTeacher;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ImageView imgPicture;
+
+    @FXML
+    private ListView<?> lstContacts;
+
+    @FXML
+    private ListView<?> lstSubjects;
+
+    @FXML
+    private PasswordField pwdPassword;
+
+    @FXML
+    private TableView<?> tblBooks;
+
+    @FXML
+    private TableView<?> tblTeachersDetails;
+
+    @FXML
+    private TextField txtClass;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private TextField txtSubject;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnAddContactOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnAddSubjectOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnChangeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteContact(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteSubjectOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewTeacherOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
