5a3329a8420e8d68937a8b9080c3ae22eaf49bdb
tharuka-devinda <tharukadevinda96@gmail.com>
2023-04-15 14:30:36 +0530

    Teacher Class Complete

--- src/main/java/lk/ijse/dep10/students/model/Teacher.java
+++ src/main/java/lk/ijse/dep10/students/model/Teacher.java
@@ -1,4 +1,18 @@
 package lk.ijse.dep10.students.model;
 
-public class Teacher {
+import javafx.scene.image.ImageView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Teacher implements Serializable {
+    private int id;
+    private String name;
+    private String address;
+    private String grade;
+    private ArrayList<String> subjectList = new ArrayList<>();
+    private ArrayList<String> contactList = new ArrayList<>();
+    private ImageView picture;
+    private String userName;
+    private String password;
 }
