2d5a94953cb74992a0e65065b9a05b7d6512a2de
Dinindu Lokugamage <dinindu961030@gmail.com>
2023-04-17 14:06:32 +0530

    library-UI-Done

--- src/main/java/lk/ijse/dep10/students/controller/LibraryBookViewController.java
+++ src/main/java/lk/ijse/dep10/students/controller/LibraryBookViewController.java
@@ -1,4 +1,40 @@
 package lk.ijse.dep10.students.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class LibraryBookViewController {
+    public TextField txtBookID;
+    public TextField txtBookName;
+    public TextField txtBookAuthor;
+    public ComboBox cmbBookCategory;
+    public ComboBox cmbBookStatus;
+    public DatePicker dtpDateBorrowed;
+    public DatePicker dtpDateDue;
+    public TextField txtDaysOnLoan;
+    public TextField txtDaysOverdue;
+    public TextField txtLateReturnFine;
+    public ComboBox cmbMemberType;
+    public TextField txtMemberIndex;
+    public TextField txtMemberName;
+    public TextField txtMemberAddress;
+    public TextField txtMemberContact;
+    public Button btnAdd;
+    public Button btnSave;
+    public Button btnDelete;
+    public TableView tblLibrary;
+    public TextField txtSearch;
+    public Button btnSearch;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
 }
