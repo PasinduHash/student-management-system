66d95b4057e7eb09d71a6471adb634b75a4d7391
Chethini Kobewatta <ckavindya@gmail.com>
2023-04-18 00:30:01 +0530

    initial

--- src/main/java/lk/ijse/dep10/students/controller/LibraryBookViewController.java
+++ src/main/java/lk/ijse/dep10/students/controller/LibraryBookViewController.java
@@ -1,7 +1,16 @@
 package lk.ijse.dep10.students.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.dep10.students.db.DBConnection;
+import lk.ijse.dep10.students.model.Book;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LibraryBookViewController {
     public TextField txtBookID;
@@ -17,14 +26,43 @@ public class LibraryBookViewController {
     public ComboBox cmbMemberType;
     public TextField txtMemberIndex;
     public TextField txtMemberName;
-    public TextField txtMemberAddress;
     public TextField txtMemberContact;
     public Button btnAdd;
     public Button btnSave;
     public Button btnDelete;
-    public TableView tblLibrary;
+    public TableView<Book> tblLibrary;
     public TextField txtSearch;
     public Button btnSearch;
+
+
+    public void initialize(){
+        tblLibrary.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("bookId"));
+        tblLibrary.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("bookName"));
+        tblLibrary.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("author"));
+        tblLibrary.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("status"));
+        tblLibrary.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("dateDue"));
+
+        loadAllBooks();
+
+    }
+
+    private void loadAllBooks() {
+        try {
+            Connection connection= DBConnection.getInstance().getConnection();
+
+            Statement stm= connection.createStatement();
+            ResultSet rst=stm.executeQuery("SELECT *FROM Library");
+            ObservableList<Book> bookList= tblLibrary.getItems();
+
+            while (rst.next()){
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     public void btnAddOnAction(ActionEvent actionEvent) {
     }
--- src/main/java/lk/ijse/dep10/students/model/Book.java
+++ src/main/java/lk/ijse/dep10/students/model/Book.java
@@ -1,4 +1,25 @@
 package lk.ijse.dep10.students.model;
 
-public class Book {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Data
+
+public class Book implements Serializable {
+    private String bookId;
+    private String bookName;
+    private String author;
+    private String category;
+    private String status;
+    private LocalDate dateBorrowed;
+    private LocalDate dateDue;
+    private int daysOnLoan;
+    private int daysOverdue;
+    private double lateReturnFine;
+    private String borrowedStudentId;
+
 }
